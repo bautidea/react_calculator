e4b9fba32bb2a5b7e2e4363fd99ca0754890a7f5
bautidea <juanbautistadeangelis@gmail.com>
2023-11-30 13:55:51 -0300

    Trying to implement 'useReducer'

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,46 @@
+import { useReducer } from 'react';
 import './styles.css';
 
+const reducer = ({ currentOperand, previousOperand, operator }, action) => {
+  switch (action.type) {
+    case 'addNumber': {
+      if (currentOperand)
+        return {
+          currentOperand: Number(`${currentOperand}${action.numberToAdd}`),
+        };
+      return { currentOperand: Number(action.numberToAdd) };
+    }
+  }
+};
+
 function App() {
+  const [{ currentOperand, previousOperand, operator }, dispatch] = useReducer(
+    reducer,
+    {}
+  );
+
+  const handleNumberClick = (number) => {
+    dispatch({
+      type: 'addNumber',
+      numberToAdd: number,
+    });
+  };
+
   return (
     <>
       <div className="calculator">
         <div className="output">
-          <div className="previous-operand">5422 *</div>
-          <div className="current-operand">5555555555555555555555555</div>
+          <div className="previous-operand">
+            {previousOperand} {operator}
+          </div>
+          <div className="current-operand">{currentOperand}</div>
         </div>
         <button className="span-two">AC</button>
         <button>DEL</button>
         <button>÷</button>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
+        <button onClick={() => handleNumberClick(1)}>1</button>
+        <button onClick={() => handleNumberClick(2)}>2</button>
+        <button onClick={() => handleNumberClick(3)}>3</button>
         <button>*</button>
         <button>4</button>
         <button>5</button>
