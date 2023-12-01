b9766343d45f7e1118f6cae7a0e2fbc88cb4448b
bautidea <juanbautistadeangelis@gmail.com>
2023-12-01 08:52:28 -0300

    Implemented Reducer logic.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,51 @@
 import { useReducer } from 'react';
 import './styles.css';
 
-const reducer = ({ currentOperand, previousOperand, operator }, action) => {
+const reducer = (state, action) => {
   switch (action.type) {
-    case 'addNumber': {
-      if (currentOperand)
-        return {
-          currentOperand: Number(`${currentOperand}${action.numberToAdd}`),
-        };
-      return { currentOperand: Number(action.numberToAdd) };
+    case 'addDigit': {
+      return {
+        ...state,
+        currentOperand: `${state.currentOperand || ''}${action.numberToAdd}`,
+      };
+    }
+
+    case 'delDigit': {
+      return {
+        ...state,
+        currentOperand: state.currentOperand.slice(0, -1),
+      };
+    }
+
+    case 'clear': {
+      return {
+        currentOperand: '',
+        previousOperand: '',
+        operator: '',
+      };
+    }
+
+    case 'operation': {
+      if (!state.currentOperand) return { ...state };
+      return {
+        operator: action.selectedOperator,
+        previousOperand: `${state.currentOperand}`,
+        currentOperand: '',
+      };
+    }
+
+    case 'evaluate': {
+      if (!state.currentOperand || !state.operator) return { ...state };
+
+      const result = eval(
+        `${state.previousOperand} ${state.operator} ${state.currentOperand}`
+      );
+
+      return {
+        previousOperand: `${state.previousOperand} ${state.operator} ${state.currentOperand}`,
+        operator: '',
+        currentOperand: result,
+      };
     }
   }
 };
@@ -19,13 +56,6 @@ function App() {
     {}
   );
 
-  const handleNumberClick = (number) => {
-    dispatch({
-      type: 'addNumber',
-      numberToAdd: number,
-    });
-  };
-
   return (
     <>
       <div className="calculator">
@@ -35,24 +65,92 @@ function App() {
           </div>
           <div className="current-operand">{currentOperand}</div>
         </div>
-        <button className="span-two">AC</button>
-        <button>DEL</button>
-        <button>÷</button>
-        <button onClick={() => handleNumberClick(1)}>1</button>
-        <button onClick={() => handleNumberClick(2)}>2</button>
-        <button onClick={() => handleNumberClick(3)}>3</button>
-        <button>*</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>+</button>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button>-</button>
-        <button>.</button>
-        <button>0</button>
-        <button className="span-two">=</button>
+
+        <button
+          className="span-two"
+          onClick={() => dispatch({ type: 'clear' })}
+        >
+          AC
+        </button>
+
+        <button onClick={() => dispatch({ type: 'delDigit' })}>DEL</button>
+
+        <button
+          onClick={() => dispatch({ type: 'operation', selectedOperator: '/' })}
+        >
+          ÷
+        </button>
+
+        <button onClick={() => dispatch({ type: 'addDigit', numberToAdd: 7 })}>
+          7
+        </button>
+
+        <button onClick={() => dispatch({ type: 'addDigit', numberToAdd: 8 })}>
+          8
+        </button>
+
+        <button onClick={() => dispatch({ type: 'addDigit', numberToAdd: 9 })}>
+          9
+        </button>
+
+        <button
+          onClick={() => dispatch({ type: 'operation', selectedOperator: '*' })}
+        >
+          *
+        </button>
+
+        <button onClick={() => dispatch({ type: 'addDigit', numberToAdd: 4 })}>
+          4
+        </button>
+
+        <button onClick={() => dispatch({ type: 'addDigit', numberToAdd: 5 })}>
+          5
+        </button>
+
+        <button onClick={() => dispatch({ type: 'addDigit', numberToAdd: 6 })}>
+          6
+        </button>
+
+        <button
+          onClick={() => dispatch({ type: 'operation', selectedOperator: '+' })}
+        >
+          +
+        </button>
+
+        <button onClick={() => dispatch({ type: 'addDigit', numberToAdd: 1 })}>
+          1
+        </button>
+
+        <button onClick={() => dispatch({ type: 'addDigit', numberToAdd: 2 })}>
+          2
+        </button>
+
+        <button onClick={() => dispatch({ type: 'addDigit', numberToAdd: 3 })}>
+          3
+        </button>
+
+        <button
+          onClick={() => dispatch({ type: 'operation', selectedOperator: '/' })}
+        >
+          -
+        </button>
+
+        <button onClick={() => dispatch({ type: 'addDigit', numberToAdd: 0 })}>
+          0
+        </button>
+
+        <button
+          onClick={() => dispatch({ type: 'addDigit', numberToAdd: '.' })}
+        >
+          .
+        </button>
+
+        <button
+          className="span-two"
+          onClick={() => dispatch({ type: 'evaluate' })}
+        >
+          =
+        </button>
       </div>
     </>
   );
